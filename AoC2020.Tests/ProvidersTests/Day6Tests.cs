77f470457038e8ebde89c6d062315bfe5c4dbf5a
Bart van Duinkerken <bart.vanduinkerken@soderbergpartners.se>
2020-12-11 13:34:42 +0100

    Implemented Day 6

---
+++ AoC2020.Tests/ProvidersTests/Day6Tests.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace AoC2020.Tests.ProvidersTests
+{
+    using Providers;
+    public class Day6Tests
+    {
+        [Fact]
+        public void When_stepA_executed_Returns_6249()
+        {
+            // Arrange
+            var solver = new Day6Solver();
+
+            // Act
+            var result = solver.StepA();
+
+            // Assert
+            Assert.Equal(6249, result);
+        }
+
+        [Fact]
+        public void When_stepB_executed_Returns_3103()
+        {
+            // Arrange
+            var solver = new Day6Solver();
+
+            // Act
+            var result = solver.StepB();
+
+            // Assert
+            Assert.Equal(3103, result);
+        }
+    }
+}
---
+++ AoC2020/Providers/Day6Solver.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.IO;
+
+namespace AoC2020.Providers
+{
+    using Abstractions;
+
+    public class Day6Solver : ISolver<int>
+    {
+        public int StepA()
+        {
+            var grouped = InputFile.Split(new string[] { "\r\n\r\n" },
+                                StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => string.Join(string.Empty, x.Split(new string[] { "\r\n" },
+                                StringSplitOptions.RemoveEmptyEntries)));
+
+            var sum = grouped.Sum(a => a.Distinct().Count());
+            return sum;
+        }
+
+        public int StepB()
+        {
+            var grouped = InputFile.Split(new string[] { "\r\n\r\n" },
+                                StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => x.Split(new string[] { "\r\n" },
+                                StringSplitOptions.RemoveEmptyEntries).Select(a => a.ToCharArray()));
+            var d = grouped.Select(GetAnsweredByAll).Sum();
+            return d;
+        }
+
+        private int GetAnsweredByAll(IEnumerable<char[]> arg1, int arg2)
+        {
+            var listCount = arg1.Count();
+            return arg1.SelectMany(a => a).ToArray()
+                 .GroupBy(a => a)
+                 .Where(x => x.Count() >= listCount)
+                 .Count();
+        }
+
+        private string InputFile => File.ReadAllText("input/Day6Input.txt");
+    }
+}
